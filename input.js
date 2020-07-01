ef8525442f697f7fd50e134bbafb0774fbfe4e48
Oluwatobii <bellotobi10@gmail.com>
2020-07-01 20:10:01 +0000

    WASD Movement association

--- input.js
+++ input.js
@@ -1,8 +1,12 @@
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -18,8 +22,18 @@ const handleUserInput = (key) => {
   if (key === "\u0003") {
     process.exit();
   }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
 };
-
-setupInput();
 
 module.exports = setupInput;
