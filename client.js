91ac56b5fadf045c0447a3b3c62c8bf85d1c99ba
Oluwatobii <bellotobi10@gmail.com>
2020-07-01 18:16:28 +0000

    Experimenting with hard-coded mssgs with setTimeout & setInterval

--- client.js
+++ client.js
@@ -19,6 +19,13 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: OTB");
+    conn.write("Move: up");
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 50);
+    setInterval(() => {
+      conn.write("Move: left");
+    }, 50);
   });
 
   return conn;
