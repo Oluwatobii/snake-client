b64b41922e6352293415f09e958704aa4b36b5c4
Oluwatobii <bellotobi10@gmail.com>
2020-07-01 18:06:08 +0000

    Adding initials/Identification to our gameplay

--- client.js
+++ client.js
@@ -16,6 +16,10 @@ const connect = function () {
   conn.on("data", (data) => {
     console.log(data);
   });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: OTB");
+  });
 
   return conn;
 };
