9a94aca0de5134d6ec36d2e92961078ed839f122
Oluwatobii <bellotobi10@gmail.com>
2020-07-01 17:30:41 +0000

    Refracturing our file

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-// THIS IS THE CLIENT SIDE
-
-const net = require("net");
-
-/**
- * Establishes connection with the game server
- */
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
