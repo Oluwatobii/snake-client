e6f2edf4d6233bab5d947267c885dfb53a828536
Oluwatobii <bellotobi10@gmail.com>
2020-07-01 20:53:21 +0000

    Clean up with the constants

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
 // THIS IS THE CLIENT SIDE
 
 const net = require("net");
+const { HOST, PORT } = require("./constants");
+// const host = require("./constants");
+// const PORT = require("./constants");
 
 /**
  * Establishes connection with the game server
  */
 const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
+  const conn = net.createConnection({ host: HOST, port: PORT });
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
